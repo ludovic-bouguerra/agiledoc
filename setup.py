87a1f8b5f0e7dc2b0baea23177c08ed06cf38000
Ludovic Bouguerra <ludovic.bouguerra@kalyzee.com>
2017-04-27 21:09:50 +0200

    Testing false

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author_email='bouguerra.ludovic@gmail.com',
     url='https://www.agile-doc.com/',
     packages=['agiledoc'],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    test_suite = 'tests',
+
 )
---
+++ tests/test_app.py
@@ -0,0 +1,5 @@
+import unittest
+
+class AppTest(unittest.TestCase):
+    def test_false(self):
+        self.assertEquals(0,1)
